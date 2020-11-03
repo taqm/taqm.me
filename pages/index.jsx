c6b26de41c14fb0724991b44040cd4d9e889932a
taqm <warabii.taqm@gmail.com>
2020-11-03 11:47:43 +0900

    :sparkles: TypeScript化

--- pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Index = () => {
-  return <h1>hello next</h1>
-}
-export default Index;
-
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+import * as React from 'react';
+
+const Index: NextPage = () => {
+  return <h1>hello next</h1>
+};
+
+Index.displayName = 'pages/Index';
+
+export default Index;
+
