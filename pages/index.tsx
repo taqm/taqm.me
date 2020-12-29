5784450f52fa70028ea46eff8efeb111aa4adf9d
taqm <warabii.taqm@gmail.com>
2020-12-29 18:25:23 +0900

    :sparkles: Indexページのテンプレート化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,27 @@
 import { NextPage, GetStaticProps } from 'next';
-import Link from 'next/link';
 import * as React from 'react';
 import { loadPostFromMDX, Post } from '../src/domains/Post';
 import * as fs from 'fs';
-import MyHeader from '../src/components/MyHeader';
+import IndexPageTemplate from '../src/components/templates/IndexPageTemplate';
 
 type Props = {
   posts: Post[];
 };
 
 const Index: NextPage<Props> = ({ posts }) => {
-  return (
-    <div>
-      <MyHeader />
-      <h1>index page</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={'/posts/' + post.id}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <IndexPageTemplate posts={posts} />;
 };
 
 Index.displayName = 'pages/Index';
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const postPaths = await fs.promises.readdir('./posts');
-  const postMetas = postPaths.map((fp) => {
+  const posts = postPaths.map((fp) => {
     const { post } = loadPostFromMDX(fp.replace('.mdx', ''));
     return post;
   });
   return {
-    props: {
-      posts: postMetas,
-    },
+    props: { posts },
   };
 };
 
