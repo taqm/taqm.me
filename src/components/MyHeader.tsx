dfc9fef9f6fbcff9dadcc39de10392e2b432c566
taqm <warabii.taqm@gmail.com>
2021-01-01 12:30:04 +0900

    :bug: Linkへ渡すPropsが誤っていたので修正

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -4,13 +4,14 @@ import * as React from 'react';
 type Props = {
   isTopPage?: boolean;
 };
+
 const MyHeader: React.VFC<Props> = ({ isTopPage }) => {
   const Inner = isTopPage ? 'h1' : 'div';
   return (
     <header className="border-b border-gray-200 bg-white">
       <Inner className="flex items-center justify-center h-16 text-2xl">
-        <Link href="/" className="text-3xl">
-          taqm&apos;blog
+        <Link href="/">
+          <a className="text-3xl">taqm&apos;blog</a>
         </Link>
       </Inner>
     </header>
