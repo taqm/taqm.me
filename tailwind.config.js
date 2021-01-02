4cb9a1cf8c3ca15c0342dd8734df94fd0a2a66fc
taqm <warabii.taqm@gmail.com>
2021-01-02 11:26:05 +0900

    :bug: remarkで動的に付与していたCSSクラスがpurgeされていたので修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './src/components/**/*.tsx'],
+  purge: {
+    content: ['./pages/**/*.tsx', './src/components/**/*.tsx'],
+    options: {
+      safelist: [
+        'm-0',
+        'mt-2',
+        'mt-4',
+        'px-2',
+        'pt-6',
+        'pt-8',
+        'relative',
+        'absolute',
+        '-left-1',
+        '-top-1',
+        'bg-gray-300',
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
