40653692044914c1a234f57a6a433f10de290f7b
taqm <warabii.taqm@gmail.com>
2021-01-11 18:39:51 +0900

    :sparkles: 記事ページへ日付とタグを表示

--- src/components/templates/ShowPostPageTemplate.tsx
+++ src/components/templates/ShowPostPageTemplate.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import Link from 'next/link';
 import * as React from 'react';
 
 import { Post } from '../../Post';
@@ -11,7 +14,19 @@ type Props = {
 const ShowPostPageTemplate: React.VFC<Props> = ({ post, content }) => (
   <MainLayout>
     <article className="post-page">
+      <div className="text-sm text-gray-500">
+        {post.publishedAt.format('YYYY-MM-DD')}
+      </div>
       <h1 className="text-2xl font-bold">{post.title}</h1>
+      <ul className="flex flex-wrap mt-2">
+        {post.tags.map((tag) => (
+          <li key={tag} className="mr-2 bg-gray-200 hover:bg-gray-300">
+            <Link href={`/tags/${tag}`} prefetch={false}>
+              <a className="block px-2 h-full text-sm">{tag}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
       {/* eslint-disable-next-line react/no-danger */}
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </article>
